a5217392427212d20b2714380d6befbb01abf670
Benjamin Schinzel <schinzel.benjamin@gmail.com>
2016-11-05 16:07:00 +0100

    Docker Build für Dev Env

---
+++ docker/build-run-inspect.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+
+rm tehame.war
+
+cd ..
+
+echo "Bitte warten ... Maven baut Projekt ..."
+mvn clean package > docker/logs/mvn.log
+tail -n 7 docker/logs/mvn.log
+cp target/tehame.war docker/tehame.war
+cd docker
+
+echo "Bitte warten ... Docker baut Image ..."
+docker build . -t tehame/tehame:latest > logs/docker.log
+echo "---------------------------------------------"
+tail -n 1 logs/docker.log
+
+echo "---------------------------------------------"
+echo "Bitte warten ... Docker startet Container ..."
+docker run tehame/tehame:latest > logs/wfly.log &
+sleep 30s
+tail -n 5 logs/wfly.log
+
+echo "---------------------------------------------"
+echo "IP Adresse des Containers:"
+docker network inspect bridge | grep IPv4
+echo "Laufende Docker Instanzen:"
+docker ps
+
+echo "---------------------------------------------"
+echo "Befehl um Container zu beenden: docker stop <Container Name oder ID>"
