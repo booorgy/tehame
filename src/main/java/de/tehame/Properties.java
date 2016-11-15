e02079e24fb1d3f2d773cb78a84974ec5ad12d82
Benjamin Schinzel <schinzel.benjamin@gmail.com>
2016-11-15 14:58:12 +0100

    ENV MongoDB

--- src/main/java/de/tehame/Properties.java
+++ src/main/java/de/tehame/Properties.java
@@ -1,5 +1,13 @@
 package de.tehame;
 
 public class Properties {
-	public static String MONGO_URL = System.getenv("tehame");
+	public static String MONGO_URL = System.getenv("MONGO_DB_URL");
+	
+	/**
+	 * Um zu prüfen, ob ENV Variablen verfügbar sind. 
+	 * Debian: Siehe /etc/environment
+	 */
+	public static void main(String [] _) {
+		System.out.println(MONGO_URL);
+	}
 }
